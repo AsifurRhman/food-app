a37084ec2f43d555dfe0d7ddb7628bb783dbbfd1
SouravArefin <khansourav58@gmail.com>
2024-04-22 15:14:08 +0600

    profile photo update fixed

--- src/Components/Layout/UserForm.jsx
+++ src/Components/Layout/UserForm.jsx
@@ -2,15 +2,18 @@
 
 
 import {useState} from "react";
-import EditableImage from "./EditableImage";
 import AddressInputs from "./AddressInputs";
 import { UserProfile } from "../UserProfile/UserProfile";
-
+import { CldUploadButton, CldUploadWidgetResults } from 'next-cloudinary';
+import Image from "next/image";
+import NoImage from '../../no-image.png'
 export default function UserForm({ user, onSave }) {
   console.log(user,"user")
   const [userName, setUserName] = useState(user?.name || '');
   const [image, setImage] = useState(user?.image || '');
-  console.log(image,"image")
+  console.log(image, "image")
+  const [imageUrl, setImageUrl] = useState("");
+  const [publicId, setPublicId] = useState("");
   const [phone, setPhone] = useState(user?.phone || '');
   const [streetAddress, setStreetAddress] = useState(user?.streetAddress || '');
   const [postalCode, setPostalCode] = useState(user?.postalCode || '');
@@ -26,7 +29,20 @@ export default function UserForm({ user, onSave }) {
     if (propName === 'city') setCity(value);
     if (propName === 'country') setCountry(value);
   }
+  const handleImageUpload = (result) => {
+    console.log("result: ", result);
+    const info = result.info;
 
+    if ("secure_url" in info && "public_id" in info) {
+        const url = info.secure_url;
+        const public_id = info.public_id;
+      setImageUrl(url);
+      setImage(url)
+        setPublicId(public_id);
+        console.log("url: ", url);
+        console.log("public_id: ", public_id);
+    }
+};
   return (
     <div className="md:flex gap-4">
       
@@ -73,7 +89,47 @@ export default function UserForm({ user, onSave }) {
       </form>
       <div>
         <div className="p-2 rounded-lg relative max-w-[120px]">
-          <EditableImage link={image} setLink={setImage} />
+         
+          <CldUploadButton
+  onUpload={handleImageUpload}
+uploadPreset = {process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
+className = {`h-60 w-60 border-2 mt-4 border-dotted grid place-items-center bg-slate-100 rounded-md relative 
+}`} >
+             <div>
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+</svg>
+
+    </div>
+    
+    {
+  imageUrl ? (
+    <Image
+      src={imageUrl}
+      width={550}
+      height={550}
+      className="absolute object-cover inset-0"
+      alt="Image not found"
+    />
+  ) : image ? (
+    <Image
+      className="absolute object-cover inset-0"
+      src={image}
+      width={550}
+      height={550}
+      alt={'avatar'}
+    />
+  ) : (
+    <Image
+      className="absolute object-cover inset-0"
+      src={NoImage}
+      width={550}
+      height={550}
+      alt={'avatar'}
+    />
+  )
+}
+          </CldUploadButton>
         </div>
       </div>
     </div>
