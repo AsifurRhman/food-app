fe7fa96eadb583ef013787196b52853bc4227b6f
SouravArefin <khansourav58@gmail.com>
2024-05-15 17:38:22 +0600

    package.lock json

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>  <footer className="border-t p-8 text-center text-gray-500 mt-16">
+    <div>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
     &copy; 2024 All rights reserved
-  </footer></div>
+      </footer>
+    </div>
   )
 }
